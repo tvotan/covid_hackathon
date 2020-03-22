9cea2a7c2799ee47720a8736eccfd6e24a3dc1f7
Tin Votan <tin.votan@gmail.com>
2020-03-22 18:02:43 +0100

    update

--- wesupport_app_login/ios/Flutter/flutter_export_environment.sh
+++ wesupport_app_login/ios/Flutter/flutter_export_environment.sh
@@ -2,9 +2,10 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/Tin/projects/tools/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/Tin/projects/covid_hackathon/wesupport_app_login"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/Tin/projects/covid_hackathon/wesupport_app_login/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/Tin/projects/tools/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
